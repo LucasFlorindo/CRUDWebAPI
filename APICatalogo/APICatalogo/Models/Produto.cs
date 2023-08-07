155e1505ab63f0ffc68dce02e78fc456dbacc2ce
Lucas <lucasflorindoeg@gmail.com>
2023-08-07 13:15:34 -0300

    Utilizando data annotations

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,12 +1,27 @@
-﻿namespace APICatalogo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
     public class Produto
     {
-
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? Decricao { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Preco { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
